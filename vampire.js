9c919f99e562dacc56060049270bfa28e72100b1
Maddy Tan <maddytan7@gmail.com>
2022-05-20 20:41:04 +0000

    initial commit

--- vampire.js
+++ vampire.js
@@ -9,23 +9,32 @@ class Vampire {
   /** Simple tree methods **/
 
   // Adds the vampire as an offspring of this vampire
-  addOffspring(vampire) {
-
+  addOffspring(offspring) {
+    this.offspring.push(offspring);
+    offspring.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while(currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.offspring.length > vampire.offspring.length;
   }
 
   /** Stretch **/
@@ -36,8 +45,56 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let seniorVamp;
+    let juniorVamp;
+    
+    if (this === vampire) {
+      return vampire;
+    }
 
+    if (this.isMoreSeniorThan(vampire)) {
+      seniorVamp = this;
+      juniorVamp = vampire;
+    } else {
+      juniorVamp = this;
+      seniorVamp = vampire;
+    }
+
+    if (seniorVamp.isDirectAncestor(juniorVamp)) {
+      return seniorVamp;
+    } else {
+      return seniorVamp.creator.closestCommonAncestor(juniorVamp);
+    }
   }
+
+  
+  isDirectAncestor(vampire) {
+    if (this.offspring.includes(vampire)) {
+      return true;
+    } else {
+      for (let vamp of this.offspring) {
+        if (vamp.isDirectAncestor(vampire)) {
+          return true
+        }
+      }
+    }
+    return false;
+  }
+
+    // Returns the vampire object with that name, or null if no vampire exists with that name
+    vampireWithName(name) {
+
+    }
+  
+    // Returns the total number of vampires that exist
+    get totalDescendents() {
+  
+    }
+  
+    // Returns an array of all the vampires that were converted after 1980
+    get allMillennialVampires() {
+  
+    }
 }
 
 module.exports = Vampire;
